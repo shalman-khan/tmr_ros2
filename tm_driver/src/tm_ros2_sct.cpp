10572d2d4052ffda29ae2f93565cef005e8d9e42
shalmankhan <shabashkhan@artc.a-star.edu.sg>
2023-08-23 17:56:51 +0800

    servo_joint_control_tested

--- tm_driver/src/tm_ros2_sct.cpp
+++ tm_driver/src/tm_ros2_sct.cpp
@@ -167,8 +167,9 @@ bool TmSctRos2::set_positions(
     res->ok = rb;
     return rb;
 }
+
 double roundToTwoDecimalPlaces(double value) {
-    return std::round(value * 100.0) / 100.0;
+    return std::round(value * 1000.0) / 1000.0;
 }
 
 void TmSctRos2::servo_ctrl(trajectory_msgs::msg::JointTrajectory::SharedPtr msg) {
@@ -176,33 +177,55 @@ void TmSctRos2::servo_ctrl(trajectory_msgs::msg::JointTrajectory::SharedPtr msg)
     std::shared_ptr<tm_msgs::srv::SetPositions::Request> request;
 
     std::vector<double> rounded_positions;
+    std::vector<double> degrees_positions;
+
     for (const auto& pos : msg->points[0].positions) {
         rounded_positions.push_back(roundToTwoDecimalPlaces(pos));
     }
 
     // Check if positions have changed since the last iteration
     if (rounded_positions != previous_positions) {
-        request->motion_type = tm_msgs::srv::SetPositions::Request::PTP_J;
-        request->positions.push_back(rounded_positions[0]);
-        request->positions.push_back(rounded_positions[1]);
-        request->positions.push_back(rounded_positions[2]);
-        request->positions.push_back(rounded_positions[3]);
-        request->positions.push_back(rounded_positions[4]);
-        request->positions.push_back(rounded_positions[5]);
-        request->velocity = 3.0;  // rad/s
-        request->acc_time = 0.2;
-        request->blend_percentage = 10;
-        request->fine_goal = false;
+        // request->motion_type = tm_msgs::srv::SetPositions::Request::PTP_J;
+        // request->positions.push_back(rounded_positions[0]);
+        // request->positions.push_back(rounded_positions[1]);
+        // request->positions.push_back(rounded_positions[2]);
+        // request->positions.push_back(rounded_positions[3]);
+        // request->positions.push_back(rounded_positions[4]);
+        // request->positions.push_back(rounded_positions[5]);
+        // request->velocity = 3.0;  // rad/s
+        // request->acc_time = 0.2;
+        // request->blend_percentage = 10;
+        // request->fine_goal = false;
 
-        iface_.set_joint_pos_PTP(request->positions, request->velocity, request->acc_time, request->blend_percentage, request->fine_goal);
+        for (double radians : rounded_positions) {
+            double degrees = radians * (180.0 / M_PI);
+            degrees_positions.push_back(degrees);
+        }        
+
+        std::cout << "Rounded Positions: ";
+        for (double rad : rounded_positions) {
+            std::cout << rad << " ";
+        }
+        std::cout << std::endl;
+
+        std::cout << "Degrees Positions: ";
+        for (double deg : degrees_positions) {
+            std::cout << deg << " ";
+        }
+        std::cout << std::endl;
+
+        iface_.set_joint_pos_PTP(rounded_positions, 300.0, 0.001, 10, true);
         print_info("TM_ROS Servo Ctrl Message Sent");
 
         // Update the previous_positions with the current positions
         previous_positions = rounded_positions;
     } else {
+
+
         print_info("TM_ROS Servo Ctrl: Positions unchanged, skipping...");
     }
 }
+
 
 bool TmSctRos2::ask_sta(
         const std::shared_ptr<tm_msgs::srv::AskSta::Request> req,
